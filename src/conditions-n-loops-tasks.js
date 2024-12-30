4dcd0d995aa81f24e98470271b9607d9853b0e90
Eugeku <eugenekuzora@gmail.com>
2024-12-30 14:41:35 +0300

    feat: implement shuffle function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -462,8 +462,25 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let result = str;
+  for (let i = 1; i <= iterations; i += 1) {
+    let startStr = '';
+    let endStr = '';
+
+    for (let j = 0; j < str.length; j += 1) {
+      if (j % 2) {
+        endStr += result[j];
+      } else {
+        startStr += result[j];
+      }
+    }
+    result = `${startStr}${endStr}`;
+    if (result === str) {
+      return shuffleChar(str, iterations % i);
+    }
+  }
+  return result;
 }
 
 /**
